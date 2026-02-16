d30201ac55aba3c750d0bf87b007b24a9cf1a371
Abhyanshu <abhyanshuab05@gmail.com>
2026-02-17 00:55:54 +0530

    added vite folder files

---
+++ 03_JSX/script.jsx
@@ -0,0 +1,12 @@
+//jsx- JAVASCRIPT XML
+//javascript ke andar html likh skte 
+//HTML + JAVASCRIPT
+
+const h1 = <h1>Hi there</h1>
+
+//function calling 
+function Hero(){
+    console.log('Hello');
+}
+// Hero()
+<Hero />
---
+++ 04_folder/src/App.jsx
@@ -0,0 +1,5 @@
+function user(){
+  return "Hello guys"
+}
+// we are just exporting this function to main.jsx and executing it from there and rendering it inside the root div. THAT'S IT
+export default user
