4af3041cb7e6bcc5d15f3d6e4ee2b9d62eb2566f
Abhyanshu <abhyanshuab05@gmail.com>
2026-05-17 19:46:08 +0530

    added formHandler

---
+++ 10_notesWebApp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form className=''>
+        <input type="text"
+        placeholder='Enter '/>
+      </form>
+    </div>
+  )
+}
+
+export default App
